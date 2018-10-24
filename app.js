b5c6173fd5426c112ce530745af847493f2bb7c0
T-Streets <tyler.ceja5@gmail.com>
2018-10-24 00:40:24 -0700

    encodes user input for address

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const request = require('request');
 const config = require('./env');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURI(argv.address)
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=23130%20robert%20rd%20los%20angeles&key=${config.API_KEY}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.API_KEY}`,
     json: true
 }, (err, res, body) => {
     console.log(body.results[0].formatted_address) //number specifies how many spaces for indentation //leave middle param as undefinded
