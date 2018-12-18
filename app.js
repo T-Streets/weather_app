f467ab8490ae068290c6834e510195d10ad4f4d8
T-Streets <tyler.ceja5@gmail.com>
2018-12-18 00:49:40 -0800

    adds callback error handling

--- app.js
+++ app.js
@@ -15,13 +15,21 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+//encodes sting to be formatted for a URL
 const encodedAddress = encodeURI(argv.address)
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.API_KEY}`,
     json: true
-}, (err, res, body) => {
+}, (error, res, body) => {
+    if(error) {
+        console.log(body)
+        console.log('unable to connect to google servers')
+    } else if(body.results[0].location.geocodeQualityCode.indexOf('X') !== -1) {
+        console.log('Unable to find address')
+    } else if(body.status === 'OK') {
     console.log(body.results[0].formatted_address) //number specifies how many spaces for indentation //leave middle param as undefinded
     console.log(body.results[0].geometry.location.lat)
     console.log(body.results[0].geometry.location.lng)
+    }
 })
